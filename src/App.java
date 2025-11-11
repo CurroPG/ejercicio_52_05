47319bed7c1d07685cb04a47133c7f79c4118c8d
Curro Portillo Guerrero <cporgue2307@g.educaand.es>
2025-11-11 10:14:46 +0100

    ejercicio 52

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = Integer.parseInt(System.console().readLine("Introduzca un número: "));
+        int aux = numero;
+        aux = aux * 10 + 1;
+        int digito = 0;
+        int lastDigit = 0;
+        int invertido = 0;
+        
+        do{
+            digito = aux % 10;
+            invertido = invertido * 10 + digito;
+            aux /= 10;
+        }while(aux != 0);
+        
+        lastDigit = invertido % 10;
+        aux = invertido/10;
+        digito = 0;
+        int numeroFinal = 0;
+
+        do{
+            if (aux != 1) {
+                digito = aux % 10;
+                numeroFinal = numeroFinal * 10 + digito;
+                aux /= 10;    
+            }
+        }while(aux != 1);
+
+        numeroFinal = numeroFinal * 10 + lastDigit;
+        
+        System.out.printf("El número resultado es %d.", numeroFinal);
     }
 }
